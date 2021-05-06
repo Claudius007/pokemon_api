c3899ab5195c3eadc8a5ae1690e0fa4279f8c44b
Claudius007 <mofehintoluwaclaudius@gmail.com>
2021-05-06 09:28:58 -0400

    routing fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
  
-   resources :pokemons, only: [:index, :show, :update]   path: '/api/v1/pokemons'
+   resources :pokemons, only: [:index, :create, :show, :update , :destroy] , path: '/api/v1/pokemons'
 
     end
   
